c8dae0528dde9ac0ef7f0ea71de11bc12edce316
LightingTom <1452442064@qq.com>
2024-02-26 20:34:50 -0800

    Add scene change

---
+++ Rhythm/Assets/Part1 Script/SceneChange.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x > 152.23) {
+            SceneManager.LoadScene("Part2 Scene");
+        }
+    }
+}
