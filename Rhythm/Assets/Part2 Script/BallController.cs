8e7a6a61e28da04894e721194305d72f1501b371
shipengliu0128 <shipeng@usc.edu>
2024-02-29 16:30:26 -0800

    change move and add music

--- Rhythm/Assets/Part2 Script/BallController.cs
+++ Rhythm/Assets/Part2 Script/BallController.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -23,6 +23,9 @@ public class BallController : MonoBehaviour
     public BoxCollider2D childCollider; 
     public Vector2 expandedSize = new Vector2(0.03301108f, 0.3f); 
     public Vector2 originalSize = new Vector2(0.03301108f, 0.8f);
+
+
+    private float moveSpeed = 20f;//移动速度
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -33,7 +36,18 @@ public class BallController : MonoBehaviour
     private void Update()
     {
         isGrounded = Physics2D.OverlapCircle(foot.transform.position, groundCheckRadius, groundLayer);
-        AutoMoveRight();
+        //AutoMoveRight();
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+        }
+
+        Debug.Log("speed:" + moveSpeed);
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             Jump();
@@ -56,7 +70,13 @@ public class BallController : MonoBehaviour
 
     private void Jump()
     {
-        rb.velocity = new Vector2(autoMoveSpeed, jumpForce);
+        rb.velocity = new Vector2(autoMoveSpeed, jumpForce * 0.6f);
         isGrounded = false;
+
+        rb.gravityScale = 2f;  // 根据需要调整重力的大小
+        Invoke("ResetGravity", 0.5f);
     }
 }
+
+
+
