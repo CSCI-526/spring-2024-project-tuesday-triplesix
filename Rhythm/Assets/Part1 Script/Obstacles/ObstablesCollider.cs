89b8e0845a676fb8f99ca9287c1bc6701e7f7c78
Cassia <40779641+FredericaErath@users.noreply.github.com>
2024-03-04 19:27:46 -0800

    add statistics
    add statistics1

--- Rhythm/Assets/Part1 Script/Obstacles/ObstablesCollider.cs
+++ Rhythm/Assets/Part1 Script/Obstacles/ObstablesCollider.cs
@@ -1,18 +1,66 @@
-using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+
 
 public class ObstablesCollider : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [System.Serializable]
+    public class SerializableDictionary
+    {
+        public List<SerializableKeyValuePair> keyValuePairs = new List<SerializableKeyValuePair>();
 
+        public SerializableDictionary(Dictionary<string, int> dictionary)
+        {
+            foreach (var kvp in dictionary)
+            {
+                keyValuePairs.Add(new SerializableKeyValuePair(kvp.Key, kvp.Value));
+            }
+        }
+    }
+
+    [System.Serializable]
+    public class SerializableKeyValuePair
+    {
+        public string key;
+        public int value;
+
+        public SerializableKeyValuePair(string key, int value)
+        {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    // Start is called before the first frame update
+    private static Dictionary<string, int> dictionary = new Dictionary<string, int>();
+    SerializableDictionary dict = new SerializableDictionary(dictionary);
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            other.transform.position = GameManager.Instance.respawnPoint;
+            Vector2 spawnPoint = GameManager.Instance.respawnPoint;
+            if (dictionary.ContainsKey(spawnPoint.ToString()))
+            {
+                dictionary[spawnPoint.ToString()] += 1;
+            }
+            else
+            {
+                dictionary.Add(spawnPoint.ToString(), 1);
+            }
+            other.transform.position = spawnPoint;
         }
+    }
+    
+    private void OnApplicationQuit()
+    {
+        foreach (var kvp in dictionary)
+        {
+            Debug.Log("Key: " + kvp.Key + ", Value: " + kvp.Value);
+        }
+        SerializableDictionary serializableDictionary = new SerializableDictionary(dictionary);
+        File.WriteAllText("death.json", JsonUtility.ToJson(serializableDictionary));
+        Debug.Log("quit!!");
     }
 
 }
