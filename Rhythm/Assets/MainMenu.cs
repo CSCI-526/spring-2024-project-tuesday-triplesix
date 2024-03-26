38c86b5625b2b56da30db18d1e6dc6d4be0786aa
Zhuowen Fang <zhuowenf@usc.edu>
2024-03-25 20:35:28 -0700

    Main Menu Adjustment

--- Rhythm/Assets/MainMenu.cs
+++ Rhythm/Assets/MainMenu.cs
@@ -2,14 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame ()
+    public Button playButton;
+    public GameObject LevelPanel;
+    public Button Level1Button;
+    public void Start()
     {
-        SceneManager.LoadSceneAsync(2);
+        playButton.onClick.AddListener(LevelSelection);
+        Level1Button.onClick.AddListener(Play);
     }
+    public void LevelSelection()
+    {   
+        LevelPanel.SetActive(true);
 
+    }
+    public void Play()
+    {
+        SceneManager.LoadScene("Part1 Scence");
+    }
     public void PlayTutorial()
     {
         SceneManager.LoadSceneAsync(1);
