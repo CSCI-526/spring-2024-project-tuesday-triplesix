e785de519d4ae0cd73348d58912238b50f77a6ad
stupidpig <543825440@qq.com>
2024-04-13 10:31:08 -0700

    Analytics on Damage to Boss

--- Rhythm/Assets/Part2 Script/BossManager.cs
+++ Rhythm/Assets/Part2 Script/BossManager.cs
@@ -1,17 +1,23 @@
 using UnityEngine;
 using UnityEngine.UI;
-
+using Unity.Services.Core;
+using Unity.Services.Analytics;
+using UnityEngine.Analytics;
 public class BossManager : MonoBehaviour
 {
     public float maxHealth = 100f;
     private float currentHealth;
+    private int DamageNum;
     public Image healthBarFill; // 血条填充的引用
-
-    void Start()
+    async void Start()
     {
+        await UnityServices.InitializeAsync();
+        AnalyticsService.Instance.StartDataCollection();
         currentHealth = maxHealth;
+        DamageNum = 0;
         UpdateHealthBar();
     }
+
 
     // 调用这个方法来给Boss造成伤害
     public void TakeDamage(float damage)
@@ -20,6 +26,17 @@ public class BossManager : MonoBehaviour
         currentHealth -= damage;
         currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth); // 防止生命值低于0或高于最大值
         UpdateHealthBar();
+        DamageNum += 1;
+        if (currentHealth == 0)
+        {
+            Debug.Log("content: " + DamageNum);
+            CustomEvent myEvent = new CustomEvent("DamageToBossNum")
+            {
+                { "DamageNumber", DamageNum},
+            };
+            AnalyticsService.Instance.RecordEvent(myEvent);
+        }
+        
     }
 
     // 更新血条UI
