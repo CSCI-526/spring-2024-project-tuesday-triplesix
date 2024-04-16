3684bd6b6fd22c1a4914725d74d12562a2328160
Zhuowen Fang <zhuowenf@usc.edu>
2024-04-15 17:35:34 -0700

    add victory panel for each level and modify level3

--- Rhythm/Assets/Tutorial Script/ReturnToMain.cs
+++ Rhythm/Assets/Tutorial Script/ReturnToMain.cs
@@ -4,13 +4,17 @@ using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class ReturnToMain : MonoBehaviour
-{
+{       
+    public GameObject victoryPanel;
         private void OnCollisionEnter2D(Collision2D collision)
             {
                 if (collision.gameObject.CompareTag("Player"))
                 {
                     // 碰撞到特定对象后返回主页面
-                    SceneManager.LoadScene("Main Menu");
+                    // SceneManager.LoadScene("Main Menu");
+                    victoryPanel.SetActive(true);
+                    Time.timeScale = 0f;
+
                 }
             }
 }
