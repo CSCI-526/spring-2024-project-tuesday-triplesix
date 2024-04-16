3ac0fcad532015494cad1b3adbbfe294c00cac7f
LightingTom <1452442064@qq.com>
2024-04-15 18:15:49 -0700

    add fmt def to spike; add spike in level 1

--- Rhythm/Assets/Part1 Script/SpikeController.cs
+++ Rhythm/Assets/Part1 Script/SpikeController.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class SpikeController : MonoBehaviour
 {
     public float bpm = 120f;
+    // Here for the fmt string:
+    // 0 represents spike down
+    // 1 represents spike up
+    // now only support length 4
+    public string fmt = "0101";
     private float beatInterval;
-
     private float timer = 0f;
     private int beatCount = 0;
     private float moveHeight = 0;
@@ -23,33 +27,24 @@ public class SpikeController : MonoBehaviour
     void Update()
     {
         timer += Time.deltaTime;
-        
+        int nBeats = fmt.Length;
         if (timer >= beatInterval)
         {
             timer -= beatInterval;
             beatCount++;
-            
-            if (beatCount % 2 == 0)
-            {
-                MoveUp();
-            } else {
-                MoveDown();
-            }
+            Move(fmt[beatCount % nBeats]);
         }
     }
-    public void MoveUp()
+    public void Move(char position)
     {
-        Debug.Log("move up");
-        transform.position = new Vector3(transform.position.x, 
-                                             upPos, 
-                                             transform.position.z);
-    }
-
-    public void MoveDown()
-    {
-        Debug.Log("move down");
-        transform.position = new Vector3(transform.position.x, 
+        if (position == '0') {
+            transform.position = new Vector3(transform.position.x, 
                                              downPos, 
                                              transform.position.z);
+        } else {
+            transform.position = new Vector3(transform.position.x, 
+                                             upPos, 
+                                             transform.position.z);
+        }
     }
 }
